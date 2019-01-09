b66041da510470a3c802922cd217ff05e8db7fbd
Manuel Espinoza <mfewcar@gmail.com>
2019-01-09 14:24:44 -0700

    play.js begins

---
+++ play.js
@@ -0,0 +1,13 @@
+var name = 'Manuel';
+var age = 22;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+  return {
+    'Name is ' +
+    userName + 
+    ', age is ' +
+    userAge +
+    ''
+  }
+}
